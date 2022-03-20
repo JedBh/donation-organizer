4b84726fd9100ba4790bf7b14d654d8c1e3ce0f6
unknown <jbenhod@gmail.com>
2022-03-20 15:30:15 +0200

    fix: ui button simpler

--- src/components/ui/Input.js
+++ src/components/ui/Input.js
@@ -2,18 +2,20 @@ import classes from "./Input.module.css";
 
 function Input(props) {
   return (
-    <div className={classes.container}>
-      <input
-        type={props.type}
-        name="name"
-        className={classes.question}
-        id={props.id}
-        required
-        autocomplete="off"
-      />
-      <label htmlFor={props.id}>
-        <span>{props.label}</span>
-      </label>
+    <div className={classes.relContainer}>
+      <div className={classes.container}>
+        <input
+          type={props.type}
+          name="name"
+          className={classes.question}
+          id={props.id}
+          required
+          autocomplete="off"
+        />
+        <label htmlFor={props.id}>
+          <span>{props.label}</span>
+        </label>
+      </div>
     </div>
   );
 }
